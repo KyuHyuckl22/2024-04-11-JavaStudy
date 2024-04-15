e9139641489c55b4c6271c0c21f0c28679fbc392
user <user@DESKTOP-GD2QDUE>
2024-04-15 14:58:00 +0900

    240412

--- Chapter02_변수와연산자_25/src/변수_8.java
+++ Chapter02_변수와연산자_25/src/변수_8.java
@@ -38,7 +38,7 @@ import java.util.Arrays;
 public class 변수_8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		int [] arr= {30 , 20, 40, 50, 10};
 		System.out.println("변경전");
